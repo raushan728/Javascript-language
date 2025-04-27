48b5ab9d6d7eca06c5217ff127780b1bdb867037
RAUSHAN KUMAR <raushansinghrajpoot687@gmail.com>
2025-04-27 16:25:05 +0530

    Variable

---
+++ Variable.js
@@ -0,0 +1,7 @@
+console.log("Hello i'm Raushan");
+var a = 67; // a contains 67
+console.log(a);
+a = "harry";
+console.log(a);
+// let 8raushan = 7 // Not allowed this will throw an error
+// let var = 7
