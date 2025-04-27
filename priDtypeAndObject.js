c9a94f77cc135b26eb148605d9e743c867039fc3
RAUSHAN KUMAR <raushansinghrajpoot687@gmail.com>
2025-04-27 16:24:36 +0530

    Primitive datatypes and object

---
+++ priDtypeAndObject.js
@@ -0,0 +1,23 @@
+const { type } = require("express/lib/response");
+
+// nn bb ss u - Primitives in Js
+let a = null;
+let b = 345;
+let c = true; // can also be false
+let d = BigInt("567") + BigInt("3");
+let e = "Harry";
+let f = Symbol("I am a nice symbol");
+let g;
+console.log(a, b, c, d, e, f, g);
+console.log(typeof c);
+
+// Non Primitive Data Type - Objects in Js
+const item = {
+  Raushan: true,
+  Prince: false,
+  Kunal: 67,
+  Rohan: undefined,
+};
+console.log(item["Shubh"]);
+console.log(item);
+console.log(typeof item)
